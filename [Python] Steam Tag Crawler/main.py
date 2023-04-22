ee0791bc040296ef5a7b257e9657236b00fde6dd
DO_S <rlaxogus7094@naver.com>
2023-04-22 11:03:42 +0900

    feat : json writer
    Json 형식으로도 볼 수 있음

--- [Python] Steam Tag Crawler/main.py
+++ [Python] Steam Tag Crawler/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 url = "https://store.steampowered.com/tag/browse/?l=koreana#global_492"
@@ -6,5 +7,23 @@ url = "https://store.steampowered.com/tag/browse/?l=koreana#global_492"
 html = requests.get(url)
 bs = BeautifulSoup(html.content,"html.parser")
 
+data = {}
+data['steam-tag'] = []
+
+popularity = 0
+
 for tag_list in bs.find_all('div', {'class': 'tag_browse_tag'}):
-    print(tag_list.text)
+    popularity = popularity + 1
+    steam_tag = str(tag_list.text)
+
+    print(steam_tag)
+
+    data['steam-tag'].append({
+        "popularity": popularity,
+        "tag": steam_tag
+    })
+
+json_path = "tag-list.json"
+
+with open(json_path, 'w', encoding="UTF-8") as outfile:
+    json.dump(data, outfile, indent=4, ensure_ascii=False)
